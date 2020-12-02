ec3c7208d8631c826b77ef5cce1771c93618c5d3
analuciachin <ananime@gmail.com>
2020-12-02 22:00:38 +0000

    wasd movement challenge

--- input.js
+++ input.js
@@ -3,8 +3,11 @@
  * Specifically, so that we can handle user input via stdin
  */
 
+ // Stores the active TCP connection object.
+let connection;
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -13,6 +16,15 @@ const setupInput = function() {
     stdin.on('data', (key) => {
       if (key === '\u0003') {
         process.exit();
+      } else if (key === 'w') {
+        //console.log('up');
+        conn.write('Move: up')
+      } else if (key === 'a') {
+        conn.write('Move: left');
+      } else if (key === 's') {
+        conn.write('Move: down');
+      } else if (key === 'd') {
+        conn.write('Move: right')
       }
     });
   }
